91db228a5323772c11634a2d511b003e6f1b7b41
dristic <dristic101@gmail.com>
2014-01-13 14:34:31 -0800

    Initial commit.

---
+++ app/scripts/main.js
@@ -0,0 +1,58 @@
+require.config({
+    paths: {
+        jquery: '../bower_components/jquery/jquery',
+        bootstrapAffix: '../bower_components/sass-bootstrap/js/bootstrap-affix.js',
+        bootstrapAlert: '../bower_components/sass-bootstrap/js/bootstrap-alert.js',
+        bootstrapButton: '../bower_components/sass-bootstrap/js/bootstrap-button.js',
+        bootstrapCarousel: '../bower_components/sass-bootstrap/js/bootstrap-carousel.js',
+        bootstrapCollapse: '../bower_components/sass-bootstrap/js/bootstrap-collapse.js',
+        bootstrapPopover: '../bower_components/sass-bootstrap/js/bootstrap-popover.js',
+        bootstrapScrollspy: '../bower_components/sass-bootstrap/js/bootstrap-scrollspy.js',
+        bootstrapTab: '../bower_components/sass-bootstrap/js/bootstrap-tab.js',
+        bootstrapTooltip: '../bower_components/sass-bootstrap/js/bootstrap-tooltip.js',
+        bootstrapTransition: '../bower_components/sass-bootstrap/js/bootstrap-transition.js',
+        bootstrapTypeahead: '../bower_components/sass-bootstrap/js/bootstrap-typeahead.js'
+    },
+    shim: {
+        bootstrapAffix: {
+            deps: ['jquery']
+        },
+        bootstrapAlert: {
+            deps: ['jquery']
+        },
+        bootstrapButton: {
+            deps: ['jquery']
+        },
+        bootstrapCarousel: {
+            deps: ['jquery']
+        },
+        bootstrapCollapse: {
+            deps: ['jquery']
+        },
+        bootstrapPopover: {
+            deps: ['jquery']
+        },
+        bootstrapScrollspy: {
+            deps: ['jquery']
+        },
+        bootstrapTab: {
+            deps: ['jquery']
+        },
+        bootstrapTooltip: {
+            deps: ['jquery']
+        },
+        bootstrapTransition: {
+            deps: ['jquery']
+        },
+        bootstrapTypeahead: {
+            deps: ['jquery']
+        }
+    }
+});
+
+require(['app', 'jquery'], function (app, $) {
+    'use strict';
+    // use app here
+    console.log(app);
+    console.log('Running jQuery %s', $().jquery);
+});
